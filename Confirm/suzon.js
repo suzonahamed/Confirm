8124e693e98c16c9852b1354a05e38c294aba148
suzonahamed <144415810+suzonahamed@users.noreply.github.com>
2023-09-14 21:51:30 +0600

    Add files via upload

---
+++ Confirm/suzon.js
@@ -0,0 +1,20 @@
+
+
+console.clear();
+function Nowshin()
+{
+  let yes=confirm("Do you Marry me?");
+  if(yes)
+  {
+   document.write("welcome lady and take my love");
+   // console.log("welcome lady");
+  }
+  else
+  {
+    document.write("Getout lady");
+    //console.log("Getout lady");
+  }
+
+
+}
+Nowshin();
